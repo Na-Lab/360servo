5a85592c4c92b3cc284d6fe69c10ada9f749636d
cya-ga-umai <myon.ibuki@gmail.com>
2017-12-04 16:14:17 +0900

    [cange]パルス幅の変更

--- servo_motor.cpp
+++ servo_motor.cpp
@@ -1,10 +1,12 @@
+#include <Arduino.h>
+
 #include "servo_motor.h"
 
-ServoMotor::ServoMotor() : pulseMin(1000), pulseMax(1940) {}
+ServoMotor::ServoMotor() : pulseMin(900), pulseMax(1900) {}
 
 void ServoMotor::begin() { attach(9, pulseMin, pulseMax); }
 
 void ServoMotor::turn(double angle) {
-  double pulse = ((pulseMax - pulseMin) / 360.0) * angle + pulseMin;
+  double pulse = ((pulseMax - pulseMin) / 360.) * angle + pulseMin;
   writeMicroseconds((int)pulse);
 }
